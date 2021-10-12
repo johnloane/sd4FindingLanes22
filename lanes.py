00410cf939d6bc6ea4d261dc1599f0d108fd155b
John Loane <john.loane@dkit.ie>
2021-10-12 09:56:30 +0100

    Bug Fix: I was not calling the averged lines in video
    Some of the lines have very small slopes and this was causing
    massive x values which cv2 could not draw. Needed to add check
    to make sure that slope does not get super small in either
    positive or negative directions

--- lanes.py
+++ lanes.py
@@ -25,79 +25,84 @@ def display_lines(image_of_interest, lines):
     if lines is not None:
         for line in lines:
             #print(line)
-            x1, y1, x2, y2 = line.reshape(4)
-            #print(x1, y1, x2, y2)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
+            if line is not None:
+                x1, y1, x2, y2 = line.reshape(4)
+                print(x1, y1, x2, y2)
+                cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     return line_image
 
 
 def average_slope_intercept(image, lines):
     left_fit = []
     right_fit = []
+    if lines is None:
+        return None
     for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        if slope < 0:
-            left_fit.append((slope, intercept))
-        else:
-            right_fit.append((slope, intercept))
+        for x1, y1, x2, y2 in line:
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
     if len(left_fit) and len(right_fit):
-        left_fit_average = np.average((left_fit), axis=0 )
-        right_fit_average = np.average((right_fit), axis=0)
+        left_fit_average = np.average(left_fit, axis=0 )
+        right_fit_average = np.average(right_fit, axis=0)
         #left_line = np.min(left_fit)
         left_line = make_coordinates(image, left_fit_average)
-        print("Left line: ", left_line)
         right_line = make_coordinates(image, right_fit_average)
-        print(right_line)
-        return np.array([left_line, right_line])
+        return [left_line, right_line]
 
 
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
-    y1 = 715
-    y2 = int(y1*(3/5))
-    x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
-    return np.array([x1, y1, x2, y2])
+    if line_parameters is not None and abs(slope) > 0.001:
+        y1 = image.shape[1]
+        y2 = int(y1*(9/20))
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept)/slope)
+        return np.array([x1, y1, x2, y2])
 
 
 
-#image = cv2.imread("IMG_2095.jpg")
-#image = cv2.resize(image, (960, 1280))
-#lane_image = np.copy(image)
-#canny_image = canny(lane_image)
-#roi_image = region_of_interest(canny_image)
-#cropped_image = region_of_interest(canny_image)
-#lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 100)
-#print(lines)
-#averaged_lines = average_slope_intercept(lane_image, lines)
-#print(averaged_lines)
-#line_image = display_lines(lane_image, averaged_lines)
-#combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+image = cv2.imread("IMG_2095.jpg")
+image = cv2.resize(image, (960, 1280))
+lane_image = np.copy(image)
+canny_image = canny(lane_image)
+roi_image = region_of_interest(canny_image)
+cropped_image = region_of_interest(canny_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 100)
+print(lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+print(averaged_lines)
+line_image = display_lines(lane_image, averaged_lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
-#print(image.shape)
-#image = cv2.resize(image, (854, 1281))
-#print(image.shape)
-#cv2.imshow("output", combo_image)
-#cv2.waitKey(0)
+print(image.shape)
+image = cv2.resize(image, (854, 1281))
+print(image.shape)
+cv2.imshow("output", combo_image)
+cv2.waitKey(0)
 #image = cv2.resize(image, 1280, 960)
 
-
-
-cap = cv2.VideoCapture("IMG_2094.mp4")
-
-
-while(cap.isOpened()):
-    i, frame = cap.read()
-    if frame is not None:
-        frame = cv2.resize(frame, (960, 1280))
-        lane_image = np.copy(frame)
-        canny_image = canny(lane_image)
-        cropped_image = region_of_interest(canny_image)
-        lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=100)
-        line_image = display_lines(lane_image, lines)
-        combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-        cv2.imshow("output", combo_image)
-        cv2.waitKey(1)
+# cap = cv2.VideoCapture("IMG_2094.mp4")
+#
+# while(cap.isOpened()):
+#     i, frame = cap.read()
+#     if frame is not None:
+#         frame = cv2.resize(frame, (960, 1280))
+#         lane_image = np.copy(frame)
+#         canny_image = canny(lane_image)
+#         cropped_image = region_of_interest(canny_image)
+#         lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=100)
+#         averaged_lines = average_slope_intercept(lane_image, lines)
+#         line_image = display_lines(lane_image, averaged_lines)
+#         combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+#         cv2.imshow("output", combo_image)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     else:
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
